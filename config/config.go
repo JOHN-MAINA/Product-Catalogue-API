4604687eff9522fce6efbcdf302d0e0f139c8af7
John <jqhnmaina@outlook.com>
2019-11-07 11:19:23 +0300

    get config variable from env vars

--- config/config.go
+++ config/config.go
@@ -1,11 +1,21 @@
 package config
 
-const (
-	DbName = ""
-
-	DbUser = ""
-
-	DbPass = ""
-
-	AppPort = 3001
+import (
+	"os"
+	"strconv"
 )
+
+var DbHost = os.Getenv("DB_HOST")
+
+var DbPort, _ = strconv.Atoi(os.Getenv("DB_PORT"))
+
+var DbName = os.Getenv("DB_NAME")
+
+var DbUser = os.Getenv("DB_USER")
+
+var DbPass = os.Getenv("DB_PASSWORD")
+
+// postgres mysql
+var DbDialect = os.Getenv("DB_DIALECT")
+
+var AppPort = 3001
--- database/database/database.go
+++ database/database/database.go
@@ -3,20 +3,36 @@ package database
 import (
 	"fmt"
 	"github.com/jqhnmaina/Product-Catalogue-API/config"
+	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var DB gorm.DB
 
 func Connect() gorm.DB {
+
 	defer recover()
-	dbURL := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", config.DbUser, config.DbPass, config.DbName)
-	DB, err := gorm.Open("mysql", dbURL)
+	dbURL := ""
+
+	switch config.DbDialect {
+	case "postgres":
+		dbURL = fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s", config.DbHost, config.DbPort, config.DbUser, config.DbName, config.DbPass)
+		break
+	case "mysql":
+		dbURL = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", config.DbUser, config.DbPass, config.DbName)
+		break
+	case "sqlite":
+		dbURL = config.DbName
+		break
+	}
+	DB, err := gorm.Open(config.DbDialect, dbURL)
 
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln("Failed to connect to db: " + err.Error())
 	}
 
 	return *DB
